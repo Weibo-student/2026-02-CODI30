23fa984e82817e6ed6da0895d079d41a114e184e
Weibo-student <welin@smail.pembinatrails.ca>
2026-04-07 10:32:53 -0500

    This is for my participation (add the menu method)

--- advance_sort.sh
+++ advance_sort.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
 
+ask_number() 
+{
+  read -p "Enter 10 numbers: " -a nums
+  if [ "${#nums[@]}" -ne 10 ]; then
+    echo "Please enter exactly 10 numbers!"
+    ask_number
+    return
+  fi
+}
+
+show_menu(){
+	echo "Please select a sort method!
+1. Miracle Sort
+2. Thanos Sort"
+
+	read option
+	if [ "$option" -eq 1 ] || [ "$option" -eq 2 ]; then
+        echo "You selected option $option."
+    else
+        echo "Please select a valid number!"
+    fi
+	
+}
+
+show_menu
+
+
+
+
